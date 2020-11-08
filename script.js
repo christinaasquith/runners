f712e84ac245b22c8521f31a2c388de95d4dadd6
Christina Asquith <christinaasquith@gmail.com>
2020-11-08 07:14:07 -0500

    update code

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let raceDistance = Math.floor(Math.random()* 1000);
- let register = true;
- let age = 14;
- let raceNumber = 1;
- if(register && age < 15) {
-     raceNumber += 1000;
- }
- if(register && age < 15) {
-     console.log(`You can join our runner's group, and your runner's number is: ${raceNumber}.`);
- }
 
- else if(!register && age < 15){
-     console.log(`You can't join this Runner's Group`);
- }
+function showminutes(){
+    console.log("are you working?")
+}
+
+
+
+
+
+
+
+
+
+
+// let raceDistance = Math.floor(Math.random()* 1000);
+//  let register = true;
+//  let age = 14;
+//  let raceNumber = 1;
+//  if(register && age < 15) {
+//      raceNumber += 1000;
+//  }
+//  if(register && age < 15) {
+//      console.log(`You can join our runner's group, and your runner's number is: ${raceNumber}.`);
+//  }
+
+//  else if(!register && age < 15){
+//      console.log(`You can't join this Runner's Group`);
+//  }
 
 
