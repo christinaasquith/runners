135724cb2e3cdddc8e868f4a938cfce1860f0770
Christina Asquith <christinaasquith@gmail.com>
2020-11-27 07:26:18 -0500

    linking html form to javascript file
    i'm trying to let users submit their age and get an automatic response generated by their age. I also played with style to even out the colors

--- script.js
+++ script.js
@@ -1,15 +1,78 @@
 
-function showminutes(){
-    console.log("are you working?")
+document.form["age_form"].onsubmit = function(){
+    console.log ('form submitted');
+    let youngAge = document.getElementById("age").value;
+    console.log (youngAge);
+    
+    if (youngAge <=9) {
+        document.getElementById('answer').innerHTML = 'You must have the legs of a fawn! We will keep it short';
+    }
+    else {
+        document.getElementById('answer').innerHTML = 'You are a grasshopper! We have a big activity for you';
+    }
 }
 
 
 
 
 
+//all the code below is stuff I'm practicting still and don't really understand
+function showminutes(){
+    console.log("are you working?")
+}
+ 
+document.getElementByIdById("demo").innerHTML = "Hello World!";
+
+
+let needRun = true;
+
+if (needRun) {
+    console.log("Go Running Today!");
+} else {
+    console.log("Keep on keeping on!");
+}
+
+let exerciseChoices = "Running";
+switch (exerciseChoices) {
+    case "swim":
+        console.log("Swim 30 mins");
+        break;
+    case "bike":
+        console.log("Bike an hour");
+        break;
+    case "pilates":
+        console.log("Pilates for 40 minutes");
+        break;
+    default:
+        console.log("Invalid exercise");
+        break;
+
+}
+
+let activity = "running";
+let clothingChoice = "";
+
+if (activity === "running") {
+    clothingChoice = "Bring sneakers.";
+} else if (activity === "swimming") {
+    clothingChoice = "Bring a swimsuit.";
+} else if (activity === "biking") {
+    clothingChice ="Bring a helmet.";
+} else if (activity === "pilates") {
+    clothingChoice = "Bring a mat.";
+} else {
+    console.log('Invalid activity.');
+};
+console.log(clothingChoice); 
 
 
 
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    const date = 
+}
 
 
 // let raceDistance = Math.floor(Math.random()* 1000);
